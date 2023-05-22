6abdfe35d994bffbfaef916d725200fcb231538b
AndresBriend98 <briendandres@gmail.com>
2023-05-22 20:28:09 -0300

    Update inbox.ts

--- src/routes/inbox.ts
+++ src/routes/inbox.ts
@@ -1,9 +1,21 @@
 import express, { Router } from 'express';
+import manejador from '../ManejadorCuentas';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-  res.render('inbox');
+  const correos = manejador.getManejador().get('andresbriend@anashe.ashe')?.bandeja.getBandejaDeEntrada().map(correo => {
+    return {
+      destinatario: Array.from(correo.getPara()).join(','),
+      asunto: correo.getAsunto(),
+      contenido: correo.getContenido(),
+      fecha: correo.getFecha(),
+      hora: correo.getHora(),
+      id: correo.generateId(),
+    };  
+  });
+
+  res.render('inbox', { correos: correos });
 });
 
 export default router;
