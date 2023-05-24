dfe4413bdd24ada5cca84152671715b1a2929839
Akoo <gnoattosuarezjoaquin@gmail.com>
2023-05-24 14:59:05 -0300

    A ver si se arregla

--- src/routes/inbox.ts
+++ src/routes/inbox.ts
@@ -5,14 +5,6 @@ const router = express.Router();
 
 router.get('/', (_req, res) => {
   const correos = manejador.getManejador().get('andresbriend@anashe.ashe')?.bandeja.getBandejaDeEntrada().map(correo => {
-    return {
-      destinatario: Array.from(correo.getPara()).join(','),
-      asunto: correo.getAsunto(),
-      contenido: correo.getContenido(),
-      fecha: correo.getFecha(),
-      hora: correo.getHora(),
-      id: correo.generateId(),
-    };  
   });
 
   res.render('inbox', { correos: correos });
