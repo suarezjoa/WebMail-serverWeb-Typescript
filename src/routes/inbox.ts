20590ee603a36d58281cb086f84a0bf004a50fdc
Akoo <gnoattosuarezjoaquin@gmail.com>
2023-06-12 00:48:52 -0300

    entrega responsiba

--- src/routes/inbox.ts
+++ src/routes/inbox.ts
@@ -16,6 +16,7 @@ router.get('/', (req, res) => {
   const correos = bandejaDeEntrada ? bandejaDeEntrada.map(correo =>{
     return {
       destinatario: Array.from(correo.getPara()).join(','),
+      remitente: correo.getEmisor(),
       asunto: correo.getAsunto(),
       contenido: correo.getContenido(),
       fecha: correo.getFecha(),
